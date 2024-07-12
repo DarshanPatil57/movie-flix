e560d485bf4c63e81ea57aba9575d599b02ab8a1
Darshan Patil <5303darshan@gmail.com>
2024-07-12 14:34:28 +0530

    feat: Add Firebase authentication for sign-in &  sign-up

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,12 @@
 import React, { useRef, useState } from "react";
 import Header from "./Header";
 import { checkvalidData } from "../utils/validate";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword,} from "firebase/auth";
+import { auth } from "../utils/firebase";
 
 const Login = () => {
-  const [IsSignInForm, setIsSignInForm] = useState(true);
-  const [errorMessage,setErrorMessage] = useState(null)
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const name = useRef(null);
   const email = useRef(null);
@@ -12,14 +14,48 @@ const Login = () => {
 
   const handleButtonClick = () => {
     // validstion for form data
-   const message = checkvalidData(email.current.value , password.current.value);
-   setErrorMessage(message)
+    const message = checkvalidData(email.current.value, password.current.value);
+    setErrorMessage(message);
     // console.log(email);
     // console.log(password);
+
+    if (message) return;
+    // signin signup logic
+
+    if (!isSignInForm) {
+      //sigup logic
+      createUserWithEmailAndPassword(auth, email.current.value, password.current.value,)
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          console.log(user);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode +"-" + errorMessage)
+          // ..
+        });
+    } else {
+      //signin logic
+      signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage)
+        });
+    }
   };
 
   const toggleSignUpForm = () => {
-    setIsSignInForm(!IsSignInForm);
+    setIsSignInForm(!isSignInForm);
   };
 
   return (
@@ -37,10 +73,10 @@ const Login = () => {
         className="absolute w-3/12 p-12 my-32 bg-black mx-auto right-0 left-0 text-white rounded-lg bg-opacity-75"
       >
         <h1 className="font-bold text-2xl py-4">
-          {IsSignInForm ? "Sign In" : "Sign Up"}
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
 
-        {!IsSignInForm && (
+        {!isSignInForm && (
           <input
             ref={name}
             type="text"
@@ -66,13 +102,15 @@ const Login = () => {
           className="p-2 my-4 bg-red-500 w-full rounded-lg"
           onClick={handleButtonClick}
         >
-          {IsSignInForm ? "Sign In" : "Sign Up"}
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
 
-        <p className=" text-red-500 font-semibold text-center">{errorMessage}</p>
+        <p className=" text-red-500 font-semibold text-center">
+          {errorMessage}
+        </p>
 
         <p className=" cursor-pointer py-8" onClick={toggleSignUpForm}>
-          {IsSignInForm
+          {isSignInForm
             ? "New to Netflix ? Sign Up Now"
             : "Already Registered ? Sign In Now "}
         </p>
